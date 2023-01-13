585de07f8b731cd37ee85a4f4813816c16ad4cd2
ksml21 <anthonymontoyasantos@gmail.com>
2023-01-13 12:12:00 -0500

    nuevo

---
+++ src/main/java/com/example/democonfigserver/DemoConfigServerApplication.java
@@ -0,0 +1,14 @@
+package com.example.democonfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoConfigServerApplication.class, args);
+    }
+
+
+}
